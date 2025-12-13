206054a691ee308ba218817c77539a8845c68672
AZZIOUI <azzioui2004@gmail.com>
2025-12-13 20:35:33 +0100

    Ajoutez des fichiers projet.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Solution_Magasin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
